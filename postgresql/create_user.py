af2673fa1198603c034c97fbf7df0f83135df764
yenzeDM <dmitrij.eremeev.99@gmail.com>
2024-10-05 16:25:42 +0300

    some change

--- postgresql/create_user.py
+++ postgresql/create_user.py
@@ -3,83 +3,109 @@ from psycopg2.extensions import AsIs
 from os import name
 import docker
 from time import sleep
+import re
 
 
-if name == 'posix':
-    # Доступ к бд Linux
-    file_path = '/opt/recfaces/DATA/db/pg_hba.conf'
-    access_to_database = 'host all all all scram-sha-256\n'
-    
-    with open(file_path, 'r') as file:
-        text = file.read()
-    
-        if access_to_database not in text:
-            client = docker.from_env()
-            container_name = 'Postgres'
-            container = client.containers.get(container_name)
+def database_access():
+    if name == "posix":
+        # Доступ к бд Linux
+        file_path = "/path/to/pg_hba.conf"
+        access_to_database = "host all all all scram-sha-256\n"
+        pattern = r"host all all all scram-sha-256"
         
-            with open(file_path, 'a') as file:
-                file.writelines(access_to_database)
+        
+        with open(file_path, "r") as file:
+            text = file.read()
+            search_access = re.search(pattern, text)
+        
+            if not search_access:
+                print("Adding database access...")
+                client = docker.from_env()
+                container_name = "Postgres"
+                container = client.containers.get(container_name)
+            
+                with open(file_path, "a") as file:
+                    file.writelines(access_to_database)
+                    file.close()
+
+                container.restart()
+                sleep(5)
+            else:
+                print("Database access already exist!")
                 file.close()
+                return False
 
-            container.restart()
-            sleep(5)
+            file.close()
+            return True
 
-elif name == 'nt':
-    # Доступ к бд Windows
-    file_path = r'E:\\personal\\scripts\\postgresql\\pg_hba.conf'
-    access_to_database = 'host all all all scram-sha-256\n'
-    
-    with open(file_path, 'r') as file:
-        text = file.read()
+    elif name == "nt":
+        # Доступ к бд Windows
+        file_path = r"E:\\personal\\scripts\\postgresql\\pg_hba.conf"
+        access_to_database = "host all all all scram-sha-256\n"
+        pattern = r"host all all all scram-sha-256"
+        
+        with open(file_path, "r") as file:
+            text = file.read()
+            search_access = re.search(pattern, text)
 
-        if access_to_database not in text:
-            with open(file_path, 'a') as file:
-                file.writelines(access_to_database)
-                file.close()
+            if not search_access:
+                with open(file_path, "a") as file:
+                    file.writelines(access_to_database)
+                    file.close()
+            else:
+                print("Database access already exist!")
 
-    file_path = r'E:\\personal\\scripts\\postgresql\\postgresql.conf'
-    listen_addresses = "listen_addresses = '*'\n"
+        file_path = r'E:\\personal\\scripts\\postgresql\\postgresql.conf'
+        listen_addresses = "listen_addresses = '*'\n"
 
-    # Открытие файла для чтения
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
+        # Открытие файла для чтения
+        with open(file_path, "r") as file:
+            lines = file.readlines()
 
-    # Замена строки, содержащей часть текста
-    for i, line in enumerate(lines):
-        if 'listen_addresses' in line:
-            lines[i] = listen_addresses
+        # Замена строки, содержащей часть текста
+        for i, line in enumerate(lines):
+            if "listen_addresses" in line:
+                lines[i] = listen_addresses
 
-    # Запись изменённых строк обратно в файл
-    with open(file_path, 'w') as file:
-        file.writelines(lines)
+        # Запись изменённых строк обратно в файл
+        with open(file_path, "w") as file:
+            file.writelines(lines)
 
-# Параметры подключения к PostgreSQL
-host = "localhost"
-port = "5432"
-user = "test1"    # Пользователь с правами на создание других пользователей
-password = "test1"
-dbname = "eosan"
+def create_db_user():
+    # Параметры подключения к PostgreSQL
+    host = "localhost"
+    port = "5432"
+    user = "postgres"    # Пользователь с правами на создание других пользователей
+    password = "postgres"
+    dbname = "postgres"
 
-# Новые данные для создания пользователя
-new_db_user = "test"
-pass_for_new_user = "test"
+    # Новые данные для создания пользователя
+    new_db_user = "test"
+    pass_for_new_user = "test"
 
 
-try:
-# Подключаемся к базе данных
-    conn = connect(database=dbname, user=user, password=password, host=host, port=port)
-    conn.autocommit = True
-    cursor = conn.cursor()
+    try:
+    # Подключаемся к базе данных
+        conn = connect(database=dbname, user=user, password=password, host=host, port=port)
+        conn.autocommit = True
+        cursor = conn.cursor()
 
-    # Создание нового пользователя
-    cursor.execute(f"CREATE USER %s WITH PASSWORD %s SUPERUSER CREATEDB CREATEROLE LOGIN REPLICATION BYPASSRLS;", (AsIs(new_db_user), pass_for_new_user))    
-    print(f"Пользователь '{new_db_user}' успешно создан.")
+        # Создание нового пользователя
+        cursor.execute(f"CREATE USER %s WITH PASSWORD %s SUPERUSER CREATEDB CREATEROLE LOGIN REPLICATION BYPASSRLS;", (AsIs(new_db_user), pass_for_new_user))
+        return True
 
-except OperationalError as e:
-    print(f"Ошибка: '{e}'")
-finally:
-    if conn:
-        cursor.close()
-        conn.close()
-        print("Подключение к базе данных закрыто.")
+    except OperationalError as e:
+        print(f"Error: '{e}'")
+        return False
+    finally:
+        if conn:
+            cursor.close()
+            conn.close()
+            print("The database connection is closed.")
+
+if __name__ == '__main__':
+    if database_access():
+        print("Access added successfully")
+
+    if create_db_user():
+        print(f"The user has been successfully created.")
