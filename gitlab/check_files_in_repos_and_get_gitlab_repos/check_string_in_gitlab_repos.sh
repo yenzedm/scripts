d1a1a9f6c4bc6e53cb1a8843570a73ffacf8a5b5
yenzeDM <dmitrij.eremeev.99@gmail.com>
2025-01-11 12:33:48 +0300

    some change

--- gitlab/check_files_in_repos_and_get_gitlab_repos/check_string_in_gitlab_repos.sh
+++ gitlab/check_files_in_repos_and_get_gitlab_repos/check_string_in_gitlab_repos.sh
@@ -5,17 +5,18 @@ TEMP_DIR="gitlab_repos"
 BRANCH_NAME=$2
 
 if [[ -z "$SEARCH_STRING" || -z "$BRANCH_NAME" ]]; then
-    echo "Usage: $0 <gitlab_url> <private_token> <search_string> <branch_name>"
+    echo "Usage: $0 <search_string> <branch_name>"
     exit 1
 fi
 
 # Create temp directory for repos
 mkdir -p "$TEMP_DIR"
-cd "$TEMP_DIR" || exit 1
-
 # Get a list of repos through GitLab API
 echo "Get the list of repos..."
-repos=$(</home/eremeevda/rf_infra/templates/scripts_rf/gitlab/check_repo_urls.txt)
+repos=$(<$(pwd)/check_repo_urls.txt)
+cd "$TEMP_DIR" || exit 1
+
+# repos=$(</home/eremeevda/rf_infra/templates/scripts_rf/gitlab/check_repo_urls.txt)
 
 # Check a list is not empty
 if [ -z "$repos" ]; then
@@ -47,9 +48,9 @@ for repo in $repos; do
 
     # Return back and delete the clone repo
     cd ..
-    rm -rf "$repo_name"
+    # rm -rf "$repo_name"  !!!
 done
 
 # Delete temp directory
 cd ..
-rm -rf "$TEMP_DIR"
+# rm -rf "$TEMP_DIR" !!!!
