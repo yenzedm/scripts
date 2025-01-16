fcc14360b2790eb27e44cc822e049d352e41abd5
yenzeDM <dmitrij.eremeev.99@gmail.com>
2025-01-16 17:37:33 +0300

    some change

--- other/bash/clear_content_files_in_directory.sh
+++ other/bash/clear_content_files_in_directory.sh
@@ -2,6 +2,18 @@
 
 directory="$1"
 
+if [ "$1" == "--help" ]; then
+    echo "This script clears all contents in the files of a specified directory"
+    echo "Usage: $0 <directory>"
+    exit 1
+fi
+
+if [[ -z "$directory" ]]; then
+    echo "This script clears all contents in the files of a specified directory"
+    echo "Usage: $0 <directory>"
+    exit 1
+fi
+
 if [ -d "$directory" ]; then
     for file in "$directory"/*; do
         if [ -f "$file" ]; then 
--- other/bash/find_str.sh
+++ other/bash/find_str.sh
@@ -3,6 +3,12 @@
 DIRECTORY=$1
 SEARCH_STRING=$2
 
+if [ "$1" == "--help" ]; then
+    echo "This script clears all contents in the files of a specified directory"
+    echo "Usage: $0 <directory>"
+    exit 1
+fi
+
 if [[ -z "$DIRECTORY" || -z "$SEARCH_STRING" ]]; then
     echo "Usage: $0 <directory> <search_string>"
     exit 1
