bb053d77de68d0ac9c49eab1e3b922f6ca4233cb
yenzedm <dmitrij.eremeev.99@gmail.com>
2025-04-25 15:25:14 +0300

    review

--- other/python/growrate.py
+++ other/python/growrate.py
@@ -2,5 +2,5 @@ def calculate_cagr(start, end, years):
     return (end / start) ** (1 / years) - 1
 
 cagr = calculate_cagr(250000, 370000, 2)
-print(f"Среднегодовой доход: {cagr * 100:.2f}%")
+print(f"Average annual income: {cagr * 100:.2f}%")
 
