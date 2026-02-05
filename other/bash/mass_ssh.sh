cca8a8451249e9106c56b97fdd140b79a44492e7
yenzedm <dmitrij.eremeev.99@gmail.com>
2026-02-05 16:13:54 +0300

    review

--- other/bash/mass_ssh.sh
+++ other/bash/mass_ssh.sh
@@ -1,21 +1,37 @@
+#!/bin/bash
+
+processing=false  # processing start flag
+
 while IFS= read -r ip; do
+    # If we encounter "start", we turn on processing from the next line
+    if [[ "$ip" == "start" ]]; then
+        processing=true
+        continue
+    fi
+
+    # Skip comments and empty lines
+    [[ -z "$ip" || "$ip" =~ ^# ]] && continue
+
+    # If we encounter "stop", we exit the loop
+    if [[ "$ip" == "stop" ]]; then
+        break
+    fi
+
+    # If processing has not yet started, skip the line
+    if ! $processing; then
+        continue
+    fi
+
     echo "Collector: $ip"
-    ssh -n -o ConnectTimeout=5 -o BatchMode=yes "$ip" "$1"
-done < $2
+    if [[ -f "$1" ]]; then
+        # This looks like a file path → passed as stdin
+        ssh -o ConnectTimeout=5 -o BatchMode=yes "$ip" bash < "$1"
+    else
+        # think this is a command
+        ssh -o ConnectTimeout=5 -o BatchMode=yes "$ip" "$1"
+    fi
+done < "$2"
 
-
-######## chrony ##########
-# use: ./mass_ssh "<command>" <path/to/file>
-# path/to/file must contain IP addresses. 1 ip address 1 line
-#
-# all
-# ls -ld /etc/chrony.conf
-# sed -i 's|server|#server|g' /etc/chrony.conf
-# sed -i '1i server 10.10.10.10 iburst' /etc/chrony.conf
-# systemctl restart chronyd.service
-# chronyc sources -v
-# grep server /etc/chrony.conf
-# localectl set-locale LANG=ru_RU.UTF-8
-# source /etc/locale.conf
-
-########################
+######## use script ######
+# ./mass_ssh.sh "<command>" <file with server's names from .ssh/config>
+# ./mass_ssh.sh ./you-script.sh <file with server's names from .ssh/config>
