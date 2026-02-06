2a92372ebc7c08e9ecacc427ef66a57e33792c23
yenzedm <dmitrij.eremeev.99@gmail.com>
2026-02-06 17:13:52 +0300

    review

--- other/bash/mass_ssh.sh
+++ other/bash/mass_ssh.sh
@@ -8,7 +8,7 @@ while IFS= read -r ip; do
         processing=true
         continue
     fi
-
+    
     # Skip comments and empty lines
     [[ -z "$ip" || "$ip" =~ ^# ]] && continue
 
@@ -21,17 +21,24 @@ while IFS= read -r ip; do
     if ! $processing; then
         continue
     fi
-
+    echo "------------------------------------------"
     echo "Collector: $ip"
     if [[ -f "$1" ]]; then
         # This looks like a file path → passed as stdin
         ssh -o ConnectTimeout=5 -o BatchMode=yes "$ip" bash < "$1"
+    elif [[ " $* " =~ " --scp " ]]; then
+        what=$2
+        where=$3
+        scp $2 "$ip:$where"
+        echo "File $what copied in $where"
     else
         # think this is a command
         ssh -n -o ConnectTimeout=5 -o BatchMode=yes "$ip" "$1"
     fi
-done < "$2"
+    echo "------------------------------------------"
+done < "${@: -1}"
 
 ######## use script ######
 # ./mass_ssh.sh "<command>" <file with server's names from .ssh/config>
 # ./mass_ssh.sh ./you-script.sh <file with server's names from .ssh/config>
+# ./mass_ssh.sh --scp <wha>t <where> <file with server's names from .ssh/config>
